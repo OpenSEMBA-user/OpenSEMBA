ad05cb3a9be052daf6fb495bb78f39c707df6204
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2015-04-30 14:34:47 +0200

    Solved issues in Grid and Box revealed by the implementation of box::chop.

--- geometry/Grid.hpp
+++ geometry/Grid.hpp
@@ -15,10 +15,15 @@ Grid<D>::Grid(const BoxRD& box,
     offsetGrid_ = CVecID(0, 0, 0);
     for (Int i = 0; i < D; i++) {
         Real boxLength = box.getMax()(i) - box.getMin()(i);
-        Int nCells = ceil(boxLength / dxyz(i));
-        if (MathUtils::greater(boxLength, nCells*dxyz(i),
-                                dxyz(i), tolerance)) {
-            nCells++;
+        Int nCells;
+        if (dxyz(i) == (Real) 0.0) {
+            nCells = 1;
+        } else {
+            nCells = ceil(boxLength / dxyz(i));
+            if (MathUtils::greater(boxLength, nCells*dxyz(i),
+                    dxyz(i), tolerance)) {
+                nCells++;
+            }
         }
         pos_[i].resize(nCells+1);
         for (Int j = 0; j < nCells+1; j++) {
