cb88446fc47e0f4389031a1bce36288874a49683
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2015-05-04 20:41:31 +0200

    Fixed probes in ExporterNFDE

--- electromagneticSources/WaveportRectangular.cpp
+++ electromagneticSources/WaveportRectangular.cpp
@@ -30,12 +30,12 @@ bool WaveportRectangular::hasSameProperties(const EMSourceBase& rhs) const {
 vector<CVecR3> WaveportRectangular::getElectricWeights(
         vector<CVecR3> pos,
         const BoundTerminations& symmetries) const {
-    static const Real M_PI = acos(-1.0);
+    static const Real pi = acos(-1.0);
 
     vector<CVecR3> res(pos.size());
     pos = toLocalAxis(pos);
-    const Real m = M_PI * getMode().first / getWidth();
-    const Real n = M_PI * getMode().second / getHeight();
+    const Real m = pi * getMode().first / getWidth();
+    const Real n = pi * getMode().second / getHeight();
     if (getExcitationMode() == Waveport::TE) {
         for (UInt i = 0; i < pos.size(); i++) {
             res[i](x) = n * cos(m * pos[i](x)) * sin(n * pos[i](y));
