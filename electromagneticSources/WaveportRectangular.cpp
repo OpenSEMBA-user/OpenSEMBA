a4f3f0c101b0951d90d8ef7a77bd9dfe00a42943
damarro <damarro@correo.ugr.es>
2015-05-04 20:10:13 +0200

    Solve some Windows errors

--- electromagneticSources/WaveportRectangular.cpp
+++ electromagneticSources/WaveportRectangular.cpp
@@ -30,18 +30,20 @@ bool WaveportRectangular::hasSameProperties(const EMSourceBase& rhs) const {
 vector<CVecR3> WaveportRectangular::getElectricWeights(
         vector<CVecR3> pos,
         const BoundTerminations& symmetries) const {
+    static const Real M_PI = acos(-1.0);
+
     vector<CVecR3> res(pos.size());
     pos = toLocalAxis(pos);
     const Real m = M_PI * getMode().first / getWidth();
     const Real n = M_PI * getMode().second / getHeight();
     if (getExcitationMode() == Waveport::TE) {
-        for (uint i = 0; i < pos.size(); i++) {
+        for (UInt i = 0; i < pos.size(); i++) {
             res[i](x) = n * cos(m * pos[i](x)) * sin(n * pos[i](y));
             res[i](y) = - m * sin(m * pos[i](x)) * cos(n * pos[i](y));
             res[i](z) = (Real) 0.0;
         }
     } else {
-        for (uint i = 0; i < pos.size(); i++) {
+        for (UInt i = 0; i < pos.size(); i++) {
             res[i](x) = - m * cos(m * pos[i](x)) * sin(n * pos[i](y));
             res[i](y) = - m * sin(m * pos[i](x)) * cos(n * pos[i](y));
             res[i](z) = (Real) 0.0;
