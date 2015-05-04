6ffe5aad8d6bc631f01216bc52bedb0b66004ff6
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2015-05-04 09:58:02 +0200

    Working at ProjectFile tests

--- src/apps/test/ProjectFileTest.cpp
+++ src/apps/test/ProjectFileTest.cpp
@@ -1,26 +1,32 @@
 #include "gtest/gtest.h"
 #include "ProjectFile.h"
 
+#ifndef _WIN32
 class ProjectFileTest : public ::testing::Test {
-
+    void SetUp() {
+        file_ = ProjectFile("/usr/bin/ls");
+    }
+protected:
+    ProjectFile file_;
 };
 
 TEST_F(ProjectFileTest, BasicOperations) {
-    {
-        ProjectFile lhs("./path/to/file");
-        EXPECT_EQ(lhs, lhs);
-        EXPECT_FALSE(lhs.canOpen());
-        EXPECT_FALSE(lhs.canExecute());
-    }
-    {
-        ProjectFile lhs("dir");
-        EXPECT_TRUE(lhs.canExecute());
-    }
+    EXPECT_EQ(file_, file_);
+//    EXPECT_TRUE(file_.canOpen());
+//    EXPECT_TRUE(file_.canExecute());
+}
+
+TEST_F(ProjectFileTest, FolderOperations) {
+    EXPECT_EQ(file_.getFolder(), "/usr/bin/");
+    ProjectFile folder(file_.getFolder());
+    EXPECT_EQ(folder.getFolder(), "/");
 }
 
 TEST_F(ProjectFileTest, RelativePaths) {
-    ProjectFile lhs("./path/to/file");
     ProjectFile rhs("./path/");
-    EXPECT_EQ(lhs.relativeTo(rhs), ProjectFile("to/file"));
-    EXPECT_EQ(lhs.relativeTo(lhs), ProjectFile("./"));
+    EXPECT_EQ(file_.relativeTo(rhs), ProjectFile("to/file"));
+    EXPECT_EQ(file_.relativeTo(file_), ProjectFile("./"));
 }
+#else
+    #warning "Project file tests are not implemented for WIN32."
+#endif
