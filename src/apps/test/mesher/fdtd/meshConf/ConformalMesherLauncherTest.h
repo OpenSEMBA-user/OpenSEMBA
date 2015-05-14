47a5b922c7a2a65cd778beb548451fef223b9599
miguel <mcabello@ugr.es>
2015-05-14 10:19:46 +0200

    change stile

--- src/apps/test/mesher/fdtd/meshConf/ConformalMesherLauncherTest.h
+++ src/apps/test/mesher/fdtd/meshConf/ConformalMesherLauncherTest.h
@@ -16,8 +16,8 @@ public:
 
     void runUGRMesher(const string project, SmbData* smb) const {
 
-    	OptionsMesher optMsh;
-    	ConformalMesher *conf = NULL;
+        OptionsMesher optMsh;
+        ConformalMesher *conf = NULL;
         const SmbData* smb;
 
         conf = ConformalMesherLauncher().run(optMsh, smb, NULL);
@@ -25,41 +25,41 @@ public:
     }
 
     void compare(ProjectFile& cmshBase, ProjectFile& cmshNew) const{
-    	ifstream fileBase, fileNew;
+        ifstream fileBase, fileNew;
         cmshBase.openAsInput(fileBase);
         cmshNew.openAsInput(fileNew);
-    	while(!fileBase.eof()&&!fileNew.eof()){
-    		string lineBase, lineNew;
-    		if(!fileBase.eof()){
-    			lineBase = next_line(fileBase);
-    		}
-    		if(!fileNew.eof()){
-    			lineNew = next_line(fileNew);
-    		}
-    		EXPECT_TRUE(lineBase==lineNew);
-    	}
+        while(!fileBase.eof()&&!fileNew.eof()){
+            string lineBase, lineNew;
+            if(!fileBase.eof()){
+                lineBase = next_line(fileBase);
+            }
+            if(!fileNew.eof()){
+                lineNew = next_line(fileNew);
+            }
+            EXPECT_TRUE(lineBase==lineNew);
+        }
 
-    	if(!fileBase.eof()){
-    		//todo expect: the remaining lines are comments
-    	}
+        if(!fileBase.eof()){
+            //todo expect: the remaining lines are comments
+        }
 
 
-    	if(!fileBase.eof()){
-    		//todo expect the remaining lines are comments
-    	}
+        if(!fileBase.eof()){
+            //todo expect the remaining lines are comments
+        }
     }
 
 private:
     string next_line (ifstream& file) const {
-    	string line;
-    	file>>line;
-    	string trimLine;
-		trimLine = Parser::trim(line);
-    	while(!file.eof()&&trimLine.size()>0&&trimLine[0] == "*"){ //todo
-			file>>line;
-			string trimLine = Parser::trim(line);
-    	}
-		return trimLine;
+        string line;
+        file>>line;
+        string trimLine;
+        trimLine = Parser::trim(line);
+        while(!file.eof()&&trimLine.size()>0&&trimLine[0] == "*"){
+            file>>line;
+            string trimLine = Parser::trim(line);
+        }
+        return trimLine;
     }
 };
 
