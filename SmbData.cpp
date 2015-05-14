576eaf0638b8169a350684cdeb25a5ed62cf7ec4
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2015-05-14 17:29:24 +0200

    Fixed a bug in ConformalMesherLauncherTest::compare(...) not being able to finish after EOF had been reached.

--- SmbData.cpp
+++ SmbData.cpp
@@ -21,38 +21,24 @@ SmbData::SmbData(const SmbData& rhs)
 :   ProjectFile(rhs) {
     if (rhs.mesh != NULL) {
         mesh = rhs.mesh->cloneTo<Mesh>();
-    } else {
-        mesh == NULL;
     }
     if (rhs.grid != NULL) {
         grid = rhs.grid->cloneTo<Grid3>();
-    } else {
-        grid == NULL;
     }
     if (rhs.solverOptions != NULL) {
         solverOptions = rhs.solverOptions->cloneTo<OptionsSolver>();
-    } else {
-        solverOptions == NULL;
     }
     if (rhs.pMGroup != NULL) {
         pMGroup = rhs.pMGroup->cloneTo<GroupPhysicalModels<>>();
-    } else {
-        pMGroup == NULL;
     }
     if (rhs.emSources != NULL) {
         emSources = rhs.emSources->cloneTo<GroupEMSources<>>();
-    } else {
-        emSources == NULL;
     }
     if (rhs.outputRequests != NULL) {
         outputRequests = rhs.outputRequests->cloneTo<GroupOutRqs<>>();
-    } else {
-        outputRequests == NULL;
     }
     if (rhs.mesherOptions != NULL) {
         mesherOptions = rhs.mesherOptions->cloneTo<OptionsMesher>();
-    } else {
-        mesherOptions == NULL;
     }
 }
 
