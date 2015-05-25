674da5421e1640d9e4bc8cc32e1b0bcb07d69ae0
miguel <mcabello@ugr.es>
2015-05-25 19:49:07 +0200

    test

--- src/apps/test/mesher/fdtd/meshConf/IntersectionsGeneratorTest.cpp
+++ src/apps/test/mesher/fdtd/meshConf/IntersectionsGeneratorTest.cpp
@@ -1,9 +1,13 @@
 #include "gtest/gtest.h"
 #include "mesher/fdtd/meshConf/IntersectionsGenerator.h"
 
-class UGRMesherIntersectionsGeneratorTest : public ::testing::Test {
+class MesherIntersectionsGeneratorTest : public ::testing::Test {
 public:
-    UGRMesherIntersectionsGeneratorTest(){
+    MesherIntersectionsGeneratorTest() {}
+    ~MesherIntersectionsGeneratorTest() {};
+
+protected:
+    void SetUp() {
         //create grid
         CVecR3 min_  = CVecR3(0.0, 0.0, 0.0);
         CVecR3 max_  = CVecR3(5.0, 5.0, 5.0);
@@ -12,33 +16,33 @@ public:
 
         //create uMesh
         vector<CoordR3*> coords;
-        coords.push_back(new CoordR3(CoordinateId(1), CVecR3(0.0, 0.0, 0.0)));
-        coords.push_back(new CoordR3(CoordinateId(2), CVecR3(1.0, 0.0, 0.0)));
-        coords.push_back(new CoordR3(CoordinateId(3), CVecR3(0.0, 3.0, 0.0)));
+        coords.push_back(new CoordR3(CoordinateId(1), CVecR3(0.9, 0.9, 0.5)));
+        coords.push_back(new CoordR3(CoordinateId(2), CVecR3(3.1, 0.0, 0.5)));
+        coords.push_back(new CoordR3(CoordinateId(3), CVecR3(0.0, 3.1, 0.5)));
         cG_ = CoordR3Group(coords);
         vector<ElemR*> elems;
         CoordinateId vId[3] = {CoordinateId(1), CoordinateId(2), CoordinateId(3)};
         elems.push_back(new Tri3(cG_, ElementId(1), vId));
-        elems.push_back(new Tri3(cG_, ElementId(2), vId));
+//        elems.push_back(new Tri3(cG_, ElementId(2), vId));
         eG_ = ElemRGroup(elems);
 
         uMesh_ = new MeshUnstructured (cG_, eG_);
-
-
     };
 
-    ~UGRMesherIntersectionsGeneratorTest(){};
+private:
     const Grid3*   grid_;
     CoordR3Group   cG_;
     ElemRGroup     eG_;
     MeshUnstructured*  uMesh_;
 };
 
-TEST_F(UGRMesherIntersectionsGeneratorTest, BasicTests) {
+TEST_F(MesherIntersectionsGeneratorTest, BasicTests) {
 
     IntersectionsGenerator intersectionsGenerator;
     IntersectionsGroup* intersections;
     intersections = intersectionsGenerator(grid_, uMesh_);
 
+    EXPECT_TRUE(intersections->size()>0);
+    EXPECT_TRUE(intersections->size()==20);
 
 }
--- src/apps/test/test.cpp
+++ src/apps/test/test.cpp
@@ -11,15 +11,14 @@ GTEST_API_ int main(int argc, char **argv) {
 
   tests += "*ProjectFile*:";
   tests += "*Math*:";
-//  tests += "*Geometry*:";
+  tests += "*Geometry*:";
 //  tests += "*ParserSTL*:";
-//  tests += "*Mesher*:";
+  tests += "*Mesher*:";
 //  tests += "*ConformalMesherLauncherTest*:";
-  tests += "*EMSource*:";
+//  tests += "*EMSource*:";
 //  tests += "*ParserGid*:";
 //  tests += "*AdapterFDTDTest.OpenFOAMConversion*:";
 //  tests += "*AdapterFDTDTest.UGRMesherConversion*:";
-    tests += "*UGRMesherIntersectionsGeneratorTest*:";
 
   ::testing::GTEST_FLAG(filter) = tests.c_str();
 
