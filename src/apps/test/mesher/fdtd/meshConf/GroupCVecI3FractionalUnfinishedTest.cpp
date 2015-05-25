7dfe60ac04ae2098d428d57fd6037834032e5ab9
miguel <mcabello@ugr.es>
2015-05-25 17:06:06 +0200

    test

--- src/apps/test/mesher/fdtd/meshConf/GroupCVecI3FractionalUnfinishedTest.cpp
+++ src/apps/test/mesher/fdtd/meshConf/GroupCVecI3FractionalUnfinishedTest.cpp
@@ -1,27 +1,27 @@
-#include "GroupCVecI3FractionalUnfinishedTest.h"
-
-
-void MesherGroupCVecI3UnfinishedTest::SetUp() {
-
-}
-
-TEST_F(MesherGroupCVecI3UnfinishedTest, BasicOperations){
-//    EXPECT_EQ(fracPosWithElems_, fracPosWithElems_);
-//    EXPECT_EQ(fracPosEmpty_, fracPosEmpty_);
-//    EXPECT_NE(fracPosWithElems_, fracPosEmpty_);
+//#include "GroupCVecI3FractionalUnfinishedTest.h"
 //
-//    CVecI3FracU copyConstructed(fracPosWithElems_);
-//    EXPECT_EQ(fracPosWithElems_, copyConstructed);
 //
-//    CVecI3FracU assigned;
-//    assigned = fracPosWithElems_;
-//    EXPECT_EQ(fracPosWithElems_, assigned);
-
-}
-
-TEST_F(MesherGroupCVecI3UnfinishedTest, Merging){
-//    CVecI3 intPos(2);
-//    CVecR3 relPos(0.3);
-//    CVecI3Fractional fracPos(CVecI3(2), CVecR3(0.3));
-//    CVecI3FractionalUnfinished fracPosUnf()
-}
+//void MesherGroupCVecI3UnfinishedTest::SetUp() {
+//
+//}
+//
+//TEST_F(MesherGroupCVecI3UnfinishedTest, BasicOperations){
+////    EXPECT_EQ(fracPosWithElems_, fracPosWithElems_);
+////    EXPECT_EQ(fracPosEmpty_, fracPosEmpty_);
+////    EXPECT_NE(fracPosWithElems_, fracPosEmpty_);
+////
+////    CVecI3FracU copyConstructed(fracPosWithElems_);
+////    EXPECT_EQ(fracPosWithElems_, copyConstructed);
+////
+////    CVecI3FracU assigned;
+////    assigned = fracPosWithElems_;
+////    EXPECT_EQ(fracPosWithElems_, assigned);
+//
+//}
+//
+//TEST_F(MesherGroupCVecI3UnfinishedTest, Merging){
+////    CVecI3 intPos(2);
+////    CVecR3 relPos(0.3);
+////    CVecI3Fractional fracPos(CVecI3(2), CVecR3(0.3));
+////    CVecI3FractionalUnfinished fracPosUnf()
+//}
--- src/apps/test/mesher/fdtd/meshConf/UGRMesherIntersectionsGeneratorTest.cpp
+++ src/apps/test/mesher/fdtd/meshConf/UGRMesherIntersectionsGeneratorTest.cpp
@@ -1,13 +1,37 @@
-/*
- * UGRMesherIntersectionsGeneratorTest.cpp
- *
- *  Created on: May 22, 2015
- *      Author: Miguel D. Ruiz-Cabello N.
- */
+#include "gtest/gtest.h"
+#include "mesher/fdtd/meshConf/IntersectionsGenerator.h"
 
-#include "UGRMesherIntersectionsGeneratorTest.h"
+class UGRMesherIntersectionsGeneratorTest : public ::testing::Test {
+public:
+    UGRMesherIntersectionsGeneratorTest(){
+        intersectionsGenerator_1 = new IntersectionsGenerator;
+        coords.resize(3);
+        coords[0](0) = 0.9; coords[0](1) = 0.9; coords[0](2) = 0.5;
+        coords[1](0) = 3.1; coords[1](1) = 0.9; coords[1](2) = 0.5;
+        coords[2](0) = 0.9; coords[2](1) = 3.1; coords[2](2) = 0.5;
 
-TEST_F(UGRMesherIntersectionsGeneratorTest, Test) {
+        CVecR3 min_  = CVecR3(0.0, 0.0, 0.0);
+        CVecR3 max_  = CVecR3(5.0, 5.0, 5.0);
+        CVecR3 step_ = CVecR3(1.0, 1.0, 1.0);
+        grid_        = new Grid3(BoxR3(min_, max_), step_);
+    };
 
-    EXPECT_TRUE(true);
+    ~UGRMesherIntersectionsGeneratorTest(){};
+    const Grid3*   grid_;
+    vector<CVecR3> coords;
+    IntersectionsGenerator *intersectionsGenerator_1;
+};
+
+TEST_F(UGRMesherIntersectionsGeneratorTest, BasicTests) {
+    EXPECT_TRUE(intersectionsGenerator_1!=NULL);
+    vector<CVecR3> intersections;
+    vector<CVecR3> intersectionsTrue;
+
+    intersectionsGenerator_1 = new IntersectionsGenerator;
+    intersectionsGenerator_1->grid_ = grid_;
+    //intersectionsGenerator_1->addIntersection(coords, intersections);
+
+    intersectionsGenerator_1->addIntersectionBoundig(coords, intersections);
+
+    cout<<"joder"<<endl;
 }
