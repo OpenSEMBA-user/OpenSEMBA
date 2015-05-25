da2a9fdd208bf958c70e7fe4f3636e684a7a4a35
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2015-05-25 19:41:40 +0200

    Created Surfel conformal family.

---
+++ src/apps/test/mesher/fdtd/meshConf/pixelConf/GroupPixelsConfTest.cpp
@@ -0,0 +1,9 @@
+/*
+ * GroupPixelsConfTest.cpp
+ *
+ *  Created on: May 25, 2015
+ *      Author: luis
+ */
+
+#include "GroupPixelsConf.h"
+
---
+++ src/apps/test/mesher/fdtd/meshConf/pixelConf/VoxelConfTest.cpp
@@ -0,0 +1,9 @@
+/*
+ * VoxelConfTest.cpp
+ *
+ *  Created on: May 25, 2015
+ *      Author: luis
+ */
+
+#include "VoxelConf.h"
+
