0d766e348de5ac23f73948786e36976ff71c4b48
miguel <mcabello@ugr.es>
2015-05-26 13:32:48 +0200

    intersectorGenerator test

--- src/apps/test/mesher/fdtd/meshConf/IntersectionsGeneratorTest.cpp
+++ src/apps/test/mesher/fdtd/meshConf/IntersectionsGeneratorTest.cpp
@@ -6,41 +6,75 @@ public:
     MesherIntersectionsGeneratorTest() {}
     ~MesherIntersectionsGeneratorTest() {};
 
-protected:
-    void SetUp() {
-        //create grid
-        CVecR3 min_  = CVecR3(0.0, 0.0, 0.0);
-        CVecR3 max_  = CVecR3(5.0, 5.0, 5.0);
-        CVecR3 step_ = CVecR3(1.0, 1.0, 1.0);
-        grid_        = new Grid3(BoxR3(min_, max_), step_);
+    void init(const CVecR3 &min_,const CVecR3 &max_,
+              const CVecR3 &step_, const vector<CoordR3*> coords) {
 
+        //create grid
+        grid        = new Grid3(BoxR3(min_, max_), step_);
         //create uMesh
-        vector<CoordR3*> coords;
-        coords.push_back(new CoordR3(CoordinateId(1), CVecR3(0.5, 0.5, 0.5)));
-        coords.push_back(new CoordR3(CoordinateId(2), CVecR3(3.8, 0.5, 0.5)));
-        coords.push_back(new CoordR3(CoordinateId(3), CVecR3(0.5, 3.8, 0.5)));
         cG_ = CoordR3Group(coords);
         vector<ElemR*> elems;
         CoordinateId vId[3] = {CoordinateId(1), CoordinateId(2), CoordinateId(3)};
         elems.push_back(new Tri3(cG_, ElementId(1), vId));
-//        elems.push_back(new Tri3(cG_, ElementId(2), vId));
         eG_ = ElemRGroup(elems);
-
-        uMesh_ = new MeshUnstructured (cG_, eG_);
+        uMesh = new MeshUnstructured (cG_, eG_);
     };
 
-    const Grid3*   grid_;
+    const Grid3*   grid;
+    MeshUnstructured*  uMesh;
+
+private:
     CoordR3Group   cG_;
     ElemRGroup     eG_;
-    MeshUnstructured*  uMesh_;
+
 };
 
 TEST_F(MesherIntersectionsGeneratorTest, BasicTests) {
 
+    CVecR3 min  = CVecR3(0.0, 0.0, 0.0);
+    CVecR3 max  = CVecR3(5.0, 5.0, 5.0);
+    CVecR3 step = CVecR3(1.0, 1.0, 1.0);
+
+    vector<CoordR3*> coords;
+    coords.push_back(new CoordR3(CoordinateId(1), CVecR3(0.5, 0.5, 0.5)));
+    coords.push_back(new CoordR3(CoordinateId(2), CVecR3(3.8, 0.5, 0.5)));
+    coords.push_back(new CoordR3(CoordinateId(3), CVecR3(0.5, 3.8, 0.5)));
+
+    init(min, max, step, coords);
+
     IntersectionsGenerator intersectionsGenerator;
     IntersectionsGroup* intersections;
-    intersections = intersectionsGenerator(grid_, uMesh_);
+    intersections = intersectionsGenerator(grid, uMesh);
 
     EXPECT_TRUE(intersections->size()>0);
     EXPECT_TRUE(intersections->size()==21);
 }
+
+
+TEST_F(MesherIntersectionsGeneratorTest, BasicTests2) {
+
+    CVecR3 min  = CVecR3(0.0, 0.0, 0.0);
+    CVecR3 max  = CVecR3(5.0, 5.0, 5.0);
+    CVecR3 step = CVecR3(1.0, 1.0, 1.0);
+
+    vector<CoordR3*> coords;
+    coords.push_back(new CoordR3(CoordinateId(1), CVecR3(3.2, 2.5, 0.5)));
+    coords.push_back(new CoordR3(CoordinateId(2), CVecR3(3.8, 2.5, 0.5)));
+    coords.push_back(new CoordR3(CoordinateId(3), CVecR3(3.5, 3.5, 0.5)));
+
+    init(min, max, step, coords);
+
+    IntersectionsGenerator intersectionsGenerator;
+    IntersectionsGroup* intersections;
+    intersections = intersectionsGenerator(grid, uMesh);
+
+    EXPECT_TRUE(intersections->size()>0);
+    EXPECT_TRUE(intersections->size()==21);
+}
+
+
+
+
+
+
+
