28182b20248f360f28156062a27bec6f604c7c09
damarro <damarro@correo.ugr.es>
2015-05-27 12:53:26 +0200

    Solve a bug in Arguments

--- options/Arguments.cpp
+++ options/Arguments.cpp
@@ -33,8 +33,9 @@ Arguments::Arguments(const int argc,  const char* argv[]) {
 
 void Arguments::build(const vector<string>& argv) {
     int argc = argv.size();
-    for (Int i = 0; i < argc; i++) {
-        const string str = argv[i];
+    for (Int i = 1; i < argc; i++) {
+        string str = argv[i];
+        transform(str.begin(), str.end(), str.begin(), ::tolower);
         if (isKey(str)) {
             args_.insert(readArgument(i, argc, argv));
         }
@@ -46,7 +47,7 @@ void Arguments::build(const vector<string>& argv) {
 }
 
 Arguments::~Arguments() {
-	// TODO Auto-generated destructor stub
+
 }
 
 bool
@@ -196,8 +197,8 @@ pair<string, vector<string>> Arguments::readArgument(
     }
     for (int i = pos+1; i < argc; i++) {
         string str = argv[i];
-        string aux;
-        transform(str.begin(), str.end(), aux.begin(), ::tolower);
+        string aux = str;
+        transform(aux.begin(), aux.end(), aux.begin(), ::tolower);
         if (isKey(aux)) {
             return pair<string, vector<string>> (aux,value);
         }
