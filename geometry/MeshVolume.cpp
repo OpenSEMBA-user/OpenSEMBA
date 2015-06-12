6173ef29db3d6bf2b03efa8e529ce72250ebc22e
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2015-06-12 18:06:32 +0200

    Trying to compile cudg3d.

---
+++ geometry/MeshVolume.cpp
@@ -0,0 +1,18 @@
+/*
+ * MeshVolume.cpp
+ *
+ *  Created on: Jun 12, 2015
+ *      Author: luis
+ */
+
+#include "MeshVolume.h"
+
+MeshVolume::MeshVolume() {
+    // TODO Auto-generated constructor stub
+
+}
+
+MeshVolume::~MeshVolume() {
+    // TODO Auto-generated destructor stub
+}
+
