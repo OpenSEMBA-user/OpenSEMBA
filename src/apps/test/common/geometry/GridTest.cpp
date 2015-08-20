71c522a916d4f8ade32b478e8a3817af09e7dcd8
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2015-08-20 11:56:01 +0200

    Chop of boundaries using grid is not working properly. Refs #155 @1h30m

--- src/apps/test/common/geometry/GridTest.cpp
+++ src/apps/test/common/geometry/GridTest.cpp
@@ -69,6 +69,13 @@ TEST_F(GeometryGridTest, NumberOfCells) {
     EXPECT_EQ(grid.getNumCells(), CVecI3(2,2,2));
 }
 
+TEST_F(GeometryGridTest, PosInRange) {
+    vector<Real> posInRange = grid_.getPosInRange(x,0.17,0.27);
+    EXPECT_EQ(2, posInRange.size());
+    EXPECT_EQ(0.2, posInRange[0]);
+    EXPECT_EQ(0.25, posInRange[1]);
+}
+
 TEST_F(GeometryGridTest, GetCVecI3Fractional) {
     bool err;
     EXPECT_EQ(CVecI3Frac(CVecI3(0), CVecR3(0.0)),
