6e55f5cf467763dd8800338941570c2197fd1884
damarro <damarro@correo.ugr.es>
2015-11-29 11:50:30 +0100

    Solve a bug in Arguments when the project has spaces

--- gui/gid/semba.gid/semba.win.bat
+++ gui/gid/semba.gid/semba.win.bat
@@ -7,6 +7,7 @@ echo Project name: %1 >> %1.log
 echo Project path: %2 >> %1.log
 echo Problem type path: %3 >> %1.log
 
-set SEMBA_ARGS = -i %2\%1.dat
-echo %SEMBA% %SEMBA_ARGS% >> %1.log
-call %SEMBA% %SEMBA_ARGS% >> %1.log 2> %1.err
+set SEMBA_ARG1 = -i
+set SEMBA_ARG2 = %2\%1.dat
+echo %SEMBA% %SEMBA_ARG1% %SEMBA_ARG2% >> %1.log
+call %SEMBA% %SEMBA_ARG1% %SEMBA_ARG2% >> %1.log 2> %1.err
