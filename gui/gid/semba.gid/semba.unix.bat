f5870b2784f7c6109a501d5813c3d2db7043838a
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2015-11-30 12:16:38 +0100

    Fixes bug in semba.unix.bat file avoiding the calculation of projects with a white space.

--- gui/gid/semba.gid/semba.unix.bat
+++ gui/gid/semba.gid/semba.unix.bat
@@ -3,14 +3,19 @@
 # Errorfile: "$1.err"
 # WarningFile: "$1.wrn"
 
-SEMBA=$3/bin/semba
-echo "Path to command: " > $1.log
-echo $SEMBA >> $1.log
+PROJECT_NAME=$1
+PROJECT_PATH=$2
+PROBLEMTYPE_PATH=$3
 
-echo "Project name: " $1 >> $1.log
-echo "Project path: " $2 >> $1.log
-echo "Problem type path: " $3 >> $1.log
+SEMBA="$PROBLEMTYPE_PATH/bin/semba"
+echo "Path to command: " > "$PROJECT_NAME.log"
+echo $SEMBA >> "$PROJECT_NAME.log"
 
-SEMBA_COMMAND="$SEMBA -i $2/$1.dat"
-echo $SEMBA_COMMAND >> $1.log
-$SEMBA_COMMAND >> $1.log
+echo "Project name: " $PROJECT_NAME >> "$PROJECT_NAME.log"
+echo "Project path: " $PROJECT_PATH >> "$PROJECT_NAME.log"
+echo "Problem type path: " $PROBLEMTYPE_PATH >> "$PROJECT_NAME.log"
+
+SEMBA_COMMAND="$SEMBA -i $PROJECT_PATH/$PROJECT_NAME.dat"
+echo "$SEMBA_COMMAND"
+echo "$SEMBA_COMMAND" >> "$PROJECT_NAME.log"
+$SEMBA -i "$PROJECT_PATH/$PROJECT_NAME.dat"
