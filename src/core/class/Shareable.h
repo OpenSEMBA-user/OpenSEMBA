288d6bf41a8871c42ad90c1e53d6fb468e82cbdb
damarro <damarro@correo.ugr.es>
2016-02-04 18:55:32 +0100

    Add getSharedPtr to Class::Shareable

--- src/core/class/Shareable.h
+++ src/core/class/Shareable.h
@@ -31,6 +31,22 @@ class Shareable : public std::enable_shared_from_this<Shareable> {
 public:
     Shareable() {}
     virtual ~Shareable() {}
+
+    std::shared_ptr<Shareable> getSharedPtr() {
+        try {
+            return shared_from_this();
+        } catch (const std::bad_weak_ptr&) {
+            return std::shared_ptr<Shareable>(this);
+        }
+    }
+    std::shared_ptr<const Shareable> getSharedPtr() const {
+        try {
+            return shared_from_this();
+        } catch (const std::bad_weak_ptr&) {
+            return std::shared_ptr<const Shareable>(this);
+        }
+    }
+
 };
 
 } /* namespace Class */
