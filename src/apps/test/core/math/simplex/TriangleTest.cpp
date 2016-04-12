33c085a18eba02744064fee093d6756d41269487
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2016-04-12 09:08:24 +0200

    Adds Triangle Test.

---
+++ src/apps/test/core/math/simplex/TriangleTest.cpp
@@ -0,0 +1,33 @@
+// OpenSEMBA
+// Copyright (C) 2015 Salvador Gonzalez Garcia        (salva@ugr.es)
+//                    Luis Manuel Diaz Angulo         (lmdiazangulo@semba.guru)
+//                    Miguel David Ruiz-Cabello Nuñez (miguel@semba.guru)
+//                    Daniel Mateos Romero            (damarro@semba.guru)
+//
+// This file is part of OpenSEMBA.
+//
+// OpenSEMBA is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// OpenSEMBA is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenSEMBA. If not, see <http://www.gnu.org/licenses/>.
+#include "gtest/gtest.h"
+#include "math/simplex/Triangle.h"
+
+using namespace SEMBA;
+using namespace Math;
+
+TEST(MathMatrixSimplexTriangleTest, BasicOperations) {
+    static constexpr size_t N = 1;
+    Simplex::Triangle<N> tri;
+
+    EXPECT_EQ(Simplex::Triangle<N>::n, tri.order());
+    EXPECT_EQ(Simplex::Triangle<N>::nsc, tri.numberOfSimplexCoordinates());
+}
