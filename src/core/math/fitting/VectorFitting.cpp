9ab7ee49d812bef6fd1d4c428944beda134777f7
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2016-05-10 18:35:03 +0200

    Defines to-do tasks in a code-like manner to implement a vector fitting step.

--- src/core/math/fitting/VectorFitting.cpp
+++ src/core/math/fitting/VectorFitting.cpp
@@ -32,6 +32,51 @@ VectorFitting::VectorFitting(
 }
 
 void VectorFitting::fit() {
+    // ########################################################################
+    // ##################### STAGE 1: Pole identification #####################
+    // ########################################################################
+
+    // Define starting poles as a vector of complex conjugates -a + bi with
+    // the imaginary part linearly distributed over the frequency range of
+    // interest; i.e., for each pair of complex conjugates (see eqs 9 and 10):
+    //      1. imagParts = linspace(range(samples), number_of_poles)
+    //      2. realParts = imagParts / 100
+
+    // Define matrix A following equation (A.3), where:
+    //      s_k = sample[k].first()
+    //      f(s_k) = sample[k].second()
+    //      a_i = starting poles
+    // If a_i, a_{i+1} is a complex conjugate (they always come in pairs;
+    // otherwise, there is an error), the elements have to follow equation
+    // (A.6)
+
+    // Define column vector B following equation (A.4), where:
+    //      f(s_k) = sample.second()
+
+    // Solve AX = B (¿by singular value decomposition? Eigen!), whose elements
+    // are:
+    //      X[:N] = f residues
+    //      X[N] = d
+    //      X[N+1] = h
+    //      X[N+2:] = sigma residues <- these values are the important ones
+
+    // Define a diagonal matrix containing the starting poles, A, (see B.2)
+    // as follows:
+    //      If a_i is real => A[i,i] = a_i
+    //      If a_i, a_{i+1} is a complex conjugate =>
+    //          A[i,i] = A[i+1, i+1] = real(a_i)
+    //          A[i,i+1] = imag(a_i)
+    //          A[i+1,i] = -imag(a_i)
+    // Define a column vector, B, as follows (see B.2):
+    //      If a_i is real => B[i] = 1
+    //      If a_i, a_{i+1} is a complex conjugate => B[i] = 2; B[i+1] = 0
+    // Define a row vector, C, as follows:
+    //      If a_i is real => C[i] = sigma_residues[i]
+    //      If a_i, a_{i+1} is a complex conjugate =>
+    //          C[i] = real(sigma_residues[i])
+    //          C[i] = imag(sigma_residues[i])
+
+    // Compute the eigen values of H = A - BC
 }
 
 std::vector<VectorFitting::Sample> VectorFitting::getFittedSamples(
@@ -40,11 +85,13 @@ std::vector<VectorFitting::Sample> VectorFitting::getFittedSamples(
 }
 
 std::vector<std::complex<Real> > VectorFitting::getPoles() {
-
+    // assert(computed_ == true);
+    return poles_;
 }
 
 std::vector<std::complex<Real> > VectorFitting::getResidues() {
-
+    // assert(computed_ == true);
+    return residues_;
 }
 
 Real VectorFitting::getRMS() {
@@ -56,4 +103,3 @@ Real VectorFitting::getRMS() {
 } /* namespace SEMBA */
 
 #endif
-
