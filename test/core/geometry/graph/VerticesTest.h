2cf4a8d89c4d69a947bd3dd202465ff4095b5b0e
Luis Manuel Diaz Angulo <LM@DiazAngulo.com>
2021-09-22 19:05:52 +0200

    Moves license notes from files to a single LICENSE file.

--- test/core/geometry/graph/VerticesTest.h
+++ test/core/geometry/graph/VerticesTest.h
@@ -1,26 +1,6 @@
-// OpenSEMBA
-// Copyright (C) 2015 Salvador Gonzalez Garcia        (salva@ugr.es)
-//                    Luis Manuel Diaz Angulo         (lmdiazangulo@semba.guru)
-//                    Miguel David Ruiz-Cabello Nuñez (miguel@semba.guru)
-//                    Daniel Mateos Romero            (damarro@semba.guru)
-//
-// This file is part of OpenSEMBA.
-//
-// OpenSEMBA is free software: you can redistribute it and/or modify it under
-// the terms of the GNU Lesser General Public License as published by the Free
-// Software Foundation, either version 3 of the License, or (at your option)
-// any later version.
-//
-// OpenSEMBA is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
-// details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with OpenSEMBA. If not, see <http://www.gnu.org/licenses/>.
 
-#ifndef SRC_APPS_TEST_CORE_GEOMETRY_GRAPH_VERTICESTEST_H_
-#define SRC_APPS_TEST_CORE_GEOMETRY_GRAPH_VERTICESTEST_H_
+
+#pragma once
 
 #include "gtest/gtest.h"
 #include "geometry/graph/Vertices.h"
@@ -81,4 +61,3 @@ protected:
 
 
 
-#endif /* SRC_APPS_TEST_CORE_GEOMETRY_GRAPH_VERTICESTEST_H_ */
--- test/core/physicalModel/volume/ClassicTest.h
+++ test/core/physicalModel/volume/ClassicTest.h
@@ -1,26 +1,6 @@
-// OpenSEMBA
-// Copyright (C) 2015 Salvador Gonzalez Garcia        (salva@ugr.es)
-//                    Luis Manuel Diaz Angulo         (lmdiazangulo@semba.guru)
-//                    Miguel David Ruiz-Cabello Nuñez (miguel@semba.guru)
-//                    Daniel Mateos Romero            (damarro@semba.guru)
-//
-// This file is part of OpenSEMBA.
-//
-// OpenSEMBA is free software: you can redistribute it and/or modify it under
-// the terms of the GNU Lesser General Public License as published by the Free
-// Software Foundation, either version 3 of the License, or (at your option)
-// any later version.
-//
-// OpenSEMBA is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
-// details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with OpenSEMBA. If not, see <http://www.gnu.org/licenses/>.
 
-#ifndef SRC_APPS_TEST_CORE_PHYSICALMODEL_VOLUME_CLASSIC_TEST_H_
-#define SRC_APPS_TEST_CORE_PHYSICALMODEL_VOLUME_CLASSIC_TEST_H_
+
+#pragma once
 
 #include "gtest/gtest.h"
 #include "physicalModel/volume/Classic.h"
@@ -32,4 +12,3 @@ protected:
 
 };
 
-#endif /* SRC_APPS_TEST_CORE_PHYSICALMODEL_VOLUME_CLASSIC_TEST_H_ */
