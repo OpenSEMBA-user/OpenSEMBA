af5603d32c9ea046ee0626fd829e57d8b79958b5
Julio López Torres <julio@aircury.com>
2022-10-17 09:31:18 +0200

    Make `Multiport` a truly abstract class by removing `clone()` method. Update test.

--- test/core/physicalModel/multiport/RLCTest.cpp
+++ test/core/physicalModel/multiport/RLCTest.cpp
@@ -1,26 +1,26 @@
 #include "gtest/gtest.h"
-#include "physicalModel/multiport/Multiport.h"
+#include "physicalModel/multiport/RLC.h"
 
 namespace SEMBA {
 namespace PhysicalModel {
 namespace Multiport {
 
-class MultiportTest : public ::testing::Test {
+class RLCTest : public ::testing::Test {
 public:
 
 protected:
 
 };
 
-TEST_F(MultiportTest, ctor_and_copy)
+TEST_F(RLCTest, ctor_and_copy)
 {
-	Multiport orig(Id{ 1 }, "mp1", Multiport::Type::openCircuit);
+	RLC orig(Id{ 1 }, "mp1", Multiport::Type::openCircuit, 0.0, 0.0, 0.0);
 
 	EXPECT_EQ(Id{ 1 },                      orig.getId());
 	EXPECT_EQ("mp1",                        orig.getName());
 	EXPECT_EQ(Multiport::Type::openCircuit, orig.getType());
 
-	Multiport copied(orig);
+	RLC copied(orig);
 
 	EXPECT_EQ(Id{ 1 },                      copied.getId());
 	EXPECT_EQ("mp1",                        copied.getName());
